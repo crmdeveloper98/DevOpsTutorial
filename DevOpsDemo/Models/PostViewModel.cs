d27b3ee78410abdaaa4890510b0e32f28d3f8fb2
crm developer <crmdeveloper98@gmail.com>
2021-04-09 17:40:35 +0100

    Add validation to Model

--- DevOpsDemo/Models/PostViewModel.cs
+++ DevOpsDemo/Models/PostViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace DevOpsDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevOpsDemo.Models
 {
     public class PostViewModel
     {
         public int PostId { get; set; }
         public string Title { get; set; }
+
+        [StringLength(5000)]
         public string Description {get; set;}
         public string Author {get; set;}
     }
